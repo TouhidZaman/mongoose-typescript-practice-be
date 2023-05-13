e1fdf14fcf4782d78e246b5f71e23e2fff253041
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-05-13 15:28:39 +0600

    initial app structure added

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import colors from "colors";
+import * as dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+//Database connection
+async function main() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/mongoose-ts-practice");
+    console.log(colors.bgGreen.bold("DB connected"));
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(colors.bgRed.bold("DB connection error"), error);
+  }
+}
+
+main().catch((err) => console.log(err));
